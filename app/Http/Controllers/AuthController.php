41e7ebe7e132a33d4b3ce51cbcc5d50af9a50a77
Carlos Macaneta <carlosmacaneta@hotmail.com>
2021-11-08 20:52:53 +0200

    AuthController@register working

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,24 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function register(UserRequest $request)
+    {
+        $data = $request->validated();
+
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+    }
+
 
 }
